c781b667ebc9816ab24e9ff3f209873ffb6f7de8
ShawnTG71 <shawntg-network-i.t@outlook.com>
2023-02-20 12:11:18 -0500

    Publish to Heroku

--- backend/server.js
+++ backend/server.js
@@ -52,21 +52,6 @@ app.get('*', (req, res) =>
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-// test
-// app.get('/api/products', (req, res) => {
-//   res.send(data.products);
-// });
-
-// app.get('/api/products/slug/:slug', (req, res) => {
-//   const product = data.products.find(
-//     (x) => x.slug === req.params.slug
-//   );
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: 'Product Not Found' });
-//   }
-// });
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find(
